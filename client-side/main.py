d98a65e13a9c721f2b74fa9df0ddd48553050bbb
Yingqiao Gou <ygou6886@gmail.com>
2024-01-10 06:02:25 -0600

    Add files via upload

---
+++ client-side/main.py
@@ -0,0 +1,704 @@
+#
+# Client-side python app for photoapp, this time working with
+# web service, which in turn uses AWS S3 and RDS to implement
+# a simple photo application for photo storage and viewing.
+#
+# Project 02 for CS 310
+#
+# Authors:
+#   Prof. Joe Hummel (Solution)
+#   Northwestern University
+#   CS 310
+#
+
+import requests  # calling web service
+import jsons  # relational-object mapping
+
+import uuid
+import pathlib
+import logging
+import sys
+import os
+import base64
+
+from configparser import ConfigParser
+
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+
+
+###################################################################
+#
+# classes
+#
+class User:
+  userid: int  # these must match columns from DB table
+  email: str
+  lastname: str
+  firstname: str
+  bucketfolder: str
+
+
+class Asset:
+  assetid: int  # these must match columns from DB table
+  userid: int
+  assetname: str
+  bucketkey: str
+
+
+class BucketItem:
+  Key: str
+  LastModified: str
+  ETag: str
+  Size: int
+  StorageClass: str
+
+
+###################################################################
+#
+# prompt
+#
+def prompt():
+  """
+  Prompts the user and returns the command number
+
+  Parameters
+  ----------
+  None
+
+  Returns
+  -------
+  Command number entered by user (0, 1, 2, ...)
+  """
+  print()
+  print(">> Enter a command:")
+  print("   0 => end")
+  print("   1 => stats")
+  print("   2 => users")
+  print("   3 => assets")
+  print("   4 => download")
+  print("   5 => download and display")
+  print("   6 => bucket contents")
+  print("   7 => add user")
+  print("   8 => upload")
+
+  cmd = int(input())
+  return cmd
+
+
+###################################################################
+#
+# stats
+#
+def stats(baseurl):
+  """
+  Prints out S3 and RDS info: bucket status, # of users and 
+  assets in the database
+
+  Parameters
+  ----------
+  baseurl: baseurl for web service
+
+  Returns
+  -------
+  nothing
+  """
+
+  try:
+    #
+    # call the web service:
+    #
+    api = '/stats'
+    url = baseurl + api
+
+    res = requests.get(url)
+    #
+    # let's look at what we got back:
+    #
+    if res.status_code != 200:
+      # failed:
+      print("Failed with status code:", res.status_code)
+      print("url: " + url)
+      if res.status_code == 400:  # we'll have an error message
+        body = res.json()
+        print("Error message:", body["message"])
+      #
+      return
+
+    #
+    # deserialize and extract stats:
+    #
+    body = res.json()
+    #
+    print("bucket status:", body["message"])
+    print("# of users:", body["db_numUsers"])
+    print("# of assets:", body["db_numAssets"])
+
+  except Exception as e:
+    logging.error("stats() failed:")
+    logging.error("url: " + url)
+    logging.error(e)
+    return
+
+
+###################################################################
+#
+# users
+#
+def users(baseurl):
+  """
+  Prints out all the users in the database
+
+  Parameters
+  ----------
+  baseurl: baseurl for web service
+
+  Returns
+  -------
+  nothing
+  """
+
+  try:
+    #
+    # call the web service:
+    #
+    api = '/users'
+    url = baseurl + api
+
+    res = requests.get(url)
+
+    #
+    # let's look at what we got back:
+    #
+    if res.status_code != 200:
+      # failed:
+      print("Failed with status code:", res.status_code)
+      print("url: " + url)
+      if res.status_code == 400:  # we'll have an error message
+        body = res.json()
+        print("Error message:", body["message"])
+      #
+      return
+
+    #
+    # deserialize and extract users:
+    #
+    body = res.json()
+    #
+    # let's map each dictionary into a User object:
+    #
+    users = []
+    for row in body["data"]:
+      user = jsons.load(row, User)
+      users.append(user)
+    #
+    # Now we can think OOP:
+    #
+    for user in users:
+      print(user.userid)
+      print(" ", user.email)
+      print(" ", user.lastname, ",", user.firstname)
+      print(" ", user.bucketfolder)
+
+  except Exception as e:
+    logging.error("users() failed:")
+    logging.error("url: " + url)
+    logging.error(e)
+    return
+
+
+###################################################################
+#
+# assets
+#
+def assets(baseurl):
+  """
+  Prints out all the assets in the database
+
+  Parameters
+  ----------
+  baseurl: baseurl for web service
+
+  Returns
+  -------
+  nothing
+  """
+
+  try:
+    #
+    # call the web service:
+    #
+    api = '/assets'
+    url = baseurl + api
+
+    res = requests.get(url)
+
+    #
+    # let's look at what we got back:
+    #
+    if res.status_code != 200:
+      # failed:
+      print("Failed with status code:", res.status_code)
+      print("url: " + url)
+      if res.status_code == 400:  # we'll have an error message
+        body = res.json()
+        print("Error message:", body["message"])
+      #
+      return
+
+    #
+    # deserialize and extract assets:
+    #
+    body = res.json()
+    #
+    # let's map each dictionary into an Asset object:
+    #
+    assets = []
+    for row in body["data"]:
+      asset = jsons.load(row, Asset)
+      assets.append(asset)
+    #
+    # Now we can think OOP:
+    #
+    for asset in assets:
+      print(asset.assetid)
+      print(" ", asset.userid)
+      print(" ", asset.assetname)
+      print(" ", asset.bucketkey)
+
+  except Exception as e:
+    logging.error("assets() failed:")
+    logging.error("url: " + url)
+    logging.error(e)
+    return
+
+
+###################################################################
+#
+# download
+#
+def download(baseurl, display=False):
+  """
+  Prompts the user for an asset id, and downloads
+  that asset (image) from the bucket. Displays the
+  image after download if display param is True.
+
+  Parameters
+  ----------
+  baseurl: baseurl for web service,
+  display: optional param controlling display of image
+
+  Returns
+  -------
+  nothing
+  """
+
+  print("Enter asset id>")
+  assetid = input()
+
+  try:
+    #
+    # call the web service:
+    #
+    api = '/download'
+    url = baseurl + api + '/' + assetid
+
+    res = requests.get(url)
+
+    #
+    # let's look at what we got back:
+    #
+    if res.status_code != 200:
+      # failed:
+      print("Failed with status code:", res.status_code)
+      print("url: " + url)
+      if res.status_code == 400:  # we'll have an error message
+        body = res.json()
+        print("Error message:", body["message"])
+      #
+      return
+
+    #
+    # deserialize and extract image:
+    #
+    body = res.json()
+
+    #
+    # did we get an image back? Perhaps asset id was invalid...
+    #
+    userid = body["user_id"]
+
+    if userid == -1:
+      # no such image
+      print("No such asset...")
+      return
+
+    #
+    # we have an image:
+    #
+    assetname = body["asset_name"]
+    bucketkey = body["bucket_key"]
+
+    print("userid:", userid)
+    print("asset name:", assetname)
+    print("bucket key:", bucketkey)
+
+    bytes = base64.b64decode(body["data"])
+
+    #
+    # write the binary data to a file (as a
+    # binary file, not a text file):
+    #
+    outfile = open(assetname, "wb")
+    outfile.write(bytes)
+    outfile.close()
+
+    print("Downloaded from S3 and saved as '", assetname, "'")
+
+    if display:  # display the image?
+      image = img.imread(assetname)
+      plt.imshow(image)
+      plt.show()
+
+  except Exception as e:
+    logging.error("download() failed:")
+    logging.error("url: " + url)
+    logging.error(e)
+    return
+
+
+###################################################################
+#
+# bucket_contents
+#
+def bucket_contents(baseurl):
+  """
+  Prints out the contents of the S3 bucket
+
+  Parameters
+  ----------
+  baseurl: baseurl for web service
+
+  Returns
+  -------
+  nothing
+  """
+
+  try:
+    #
+    # call the web service:
+    #
+    api = '/bucket'
+    url = baseurl + api
+
+    #
+    # we have to loop since data is returned page
+    # by page:
+    #
+    while True:
+      #
+      res = requests.get(url)
+
+      #
+      # let's look at what we got back:
+      #
+      if res.status_code != 200:
+        # failed:
+        print("Failed with status code:", res.status_code)
+        print("url: " + url)
+        if res.status_code == 400:  # we'll have an error message
+          body = res.json()
+          print("Error message:", body["message"])
+        #
+        return
+
+      #
+      # deserialize and extract bucket contents, note
+      # that contents are coming back a page (12) at
+      # a time...
+      #
+      body = res.json()
+
+      #
+      # let's map each dictionary into a BucketItem object:
+      #
+      items = []
+      for row in body["data"]:
+        item = jsons.load(row, BucketItem)
+        items.append(item)
+
+      #
+      # do we have any data to display?
+      #
+      if len(items) == 0:
+        # no
+        break
+
+      #
+      # Now we can think OOP:
+      #
+      lastkey = None  # we have at least 1 asset, so this will get set
+
+      for item in items:
+        print(item.Key)
+        print(" ", item.LastModified)
+        print(" ", item.Size)
+        lastkey = item.Key
+
+      print("another page? [y/n]")
+      answer = input()
+
+      if answer == 'y':
+        # add parameter to url
+        url = baseurl + api
+        url += "?startafter=" + lastkey
+        #
+        continue
+      else:
+        break
+
+  except Exception as e:
+    logging.error("bucket_contents() failed:")
+    logging.error("url: " + url)
+    logging.error(e)
+    return
+
+
+###################################################################
+#
+# add_user
+#
+def add_user(baseurl):
+  """
+  Prompts the user for the new user's email,
+  last name, and first name, and then inserts
+  this user into the database.
+
+  Parameters
+  ----------
+  baseurl: baseurl for web service
+
+  Returns
+  -------
+  nothing
+  """
+
+  print("Enter user's email>")
+  email = input()
+
+  print("Enter user's last (family) name>")
+  last_name = input()
+
+  print("Enter user's first (given) name>")
+  first_name = input()
+
+  # generate unique folder name:
+  folder = str(uuid.uuid4())
+
+  try:
+    #
+    # build the data packet:
+    #
+    data = {
+      "email": email,
+      "lastname": last_name,
+      "firstname": first_name,
+      "bucketfolder": folder
+    }
+
+    #
+    # call the web service:
+    #
+    api = '/user'
+    url = baseurl + api
+
+    res = requests.put(url, json=data)
+
+    #
+    # let's look at what we got back:
+    #
+    if res.status_code != 200:
+      # failed:
+      print("Failed with status code:", res.status_code)
+      print("url: " + url)
+      if res.status_code == 400:  # we'll have an error message
+        body = res.json()
+        print("Error message:", body["message"])
+      #
+      return
+
+    #
+    # success, extract userid:
+    #
+    body = res.json()
+
+    userid = body["userid"]
+    message = body["message"]
+
+    print("User", userid, "successfully", message)
+
+  except Exception as e:
+    logging.error("add_user() failed:")
+    logging.error("url: " + url)
+    logging.error(e)
+    return
+
+
+###################################################################
+#
+# upload
+#
+def upload(baseurl):
+  """
+  Prompts the user for a local filename and user id, 
+  and uploads that asset (image) to the user's folder 
+  in the bucket. The asset is given a random, unique 
+  name. The database is also updated to record the 
+  existence of this new asset in S3.
+
+  Parameters
+  ----------
+  baseurl: baseurl for web service
+
+  Returns
+  -------
+  nothing
+  """
+
+  print("Enter local filename>")
+  local_filename = input()
+
+  if not pathlib.Path(local_filename).is_file():
+    print("Local file '", local_filename, "' does not exist...")
+    return
+
+  print("Enter user id>")
+  userid = input()
+
+  try:
+    #
+    # build the data packet:
+    #
+    infile = open(local_filename, "rb")
+    bytes = infile.read()
+    infile.close()
+
+    #
+    # now encode the image as base64. Note b64encode returns
+    # a bytes object, not a string. So then we have to convert
+    # (decode) the bytes -> string, and then we can serialize
+    # the string as JSON for upload to server:
+    #
+    data = base64.b64encode(bytes)
+    datastr = data.decode()
+
+    data = {"assetname": local_filename, "data": datastr}
+
+    #
+    # call the web service:
+    #
+    api = '/image'
+    url = baseurl + api + "/" + userid
+
+    res = requests.post(url, json=data)
+
+    #
+    # let's look at what we got back:
+    #
+    if res.status_code != 200:
+      # failed:
+      print("Failed with status code:", res.status_code)
+      print("url: " + url)
+      if res.status_code == 400:  # we'll have an error message
+        body = res.json()
+        print("Error message:", body["message"])
+      #
+      return
+
+    #
+    # success, extract userid:
+    #
+    body = res.json()
+
+    assetid = body["assetid"]
+
+    print("Image uploaded, asset id =", assetid)
+
+  except Exception as e:
+    logging.error("upload() failed:")
+    logging.error("url: " + url)
+    logging.error(e)
+    return
+
+
+#########################################################################
+# main
+#
+print('** Welcome to PhotoApp v2 **')
+print()
+
+# eliminate traceback so we just get error message:
+sys.tracebacklimit = 0
+
+#
+# what config file should we use for this session?
+#
+config_file = 'photoapp-client-config.ini'
+
+print("What config file to use for this session?")
+print("Press ENTER to use default (photoapp-client-config.ini),")
+print("otherwise enter name of config file>")
+s = input()
+
+if s == "":  # use default
+  pass  # already set
+else:
+  config_file = s
+
+#
+# does config file exist?
+#
+if not pathlib.Path(config_file).is_file():
+  print("**ERROR: config file '", config_file, "' does not exist, exiting")
+  sys.exit(0)
+
+#
+# setup base URL to web service:
+#
+configur = ConfigParser()
+configur.read(config_file)
+baseurl = configur.get('client', 'webservice')
+
+#
+# main processing loop:
+#
+cmd = prompt()
+
+while cmd != 0:
+  #
+  if cmd == 1:
+    stats(baseurl)
+  elif cmd == 2:
+    users(baseurl)
+  elif cmd == 3:
+    assets(baseurl)
+  elif cmd == 4:
+    download(baseurl)
+  elif cmd == 5:
+    download(baseurl, True)
+  elif cmd == 6:
+    bucket_contents(baseurl)
+  elif cmd == 7:
+    add_user(baseurl)
+  elif cmd == 8:
+    upload(baseurl)
+  else:
+    print("** Unknown command, try again...")
+  #
+  cmd = prompt()
+
+#
+# done
+#
+print()
+print('** done **')
